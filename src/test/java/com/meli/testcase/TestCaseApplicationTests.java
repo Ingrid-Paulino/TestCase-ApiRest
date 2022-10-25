bedb4f1b7214dedb6abd2f07bc24596038385d0f
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-10-25 12:29:28 -0300

    feito conexão e criado banco de dados

--- src/test/java/com/meli/testcase/TestCaseApplicationTests.java
+++ src/test/java/com/meli/testcase/TestCaseApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestCaseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
