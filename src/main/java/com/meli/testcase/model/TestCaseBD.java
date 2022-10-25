6d02bde1216ca4edc628696d99463c765edc0f3c
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-10-25 12:50:03 -0300

    Criado tabela testCase

---
+++ src/main/java/com/meli/testcase/model/TestCaseBD.java
@@ -0,0 +1,40 @@
+package com.meli.testcase.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "testCase")
+public class TestCaseBD {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_case;
+
+    @Column(nullable = false)
+    @NotNull(message = "The description of the testCase cannot be empty.")
+    private String description;
+
+    @Column(nullable = false)
+    @NotNull(message = "The quilates of the testCase cannot be empty.")
+    private Boolean tested;
+
+    @Column(nullable = false)
+    @NotNull(message = "The passed of the testCase cannot be empty.")
+    private Boolean passed;
+
+    @Column(nullable = false)
+    @NotNull(message = "The number_of_tries of the testCase cannot be empty.")
+    private Integer number_of_tries;
+
+    @Column(nullable = false)
+    @NotNull(message = "The last_update of the testCase cannot be empty.")
+    private Date last_update;
+}
