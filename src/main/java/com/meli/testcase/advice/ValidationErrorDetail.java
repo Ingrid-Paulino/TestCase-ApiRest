66044e11528e0443359b5c28f1d9d11620376d20
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-10-26 12:25:20 -0300

    feat: Implementado classes de validação

---
+++ src/main/java/com/meli/testcase/advice/ValidationErrorDetail.java
@@ -0,0 +1,13 @@
+package com.meli.testcase.advice;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ValidationErrorDetail {
+    private String field;
+    private String message;
+    private String rejectedValue;
+}
+
---
+++ src/main/java/com/meli/testcase/advice/exeption/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.meli.testcase.advice.exeption;
+
+public class NotFoundException extends Exception{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
